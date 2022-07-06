a1a70b423d3939267d4176a667e6d5401e9ba2ab
Гульсара <gulsara@Gulsaras-MacBook-Air.local>
2022-07-06 19:39:22 +0600

    lesson1

---
+++ lesson1/main.js
@@ -0,0 +1,66 @@
+const a = "jkankndsj";
+
+let c = 32;
+
+// console.log(c)
+
+c = "Hello!"
+
+// console.log(c)
+
+const bool = true;
+const bool2 = false;
+
+const obj = {
+    key: 'value',
+    username: 'sara',
+    data: {
+        first_name: "sara",
+        last_name: "mamyrova"
+    }
+}
+
+// console.log(obj.data.last_name)
+
+const arr = ["Aydana", true, false, 25]
+
+console.log(arr[2]);
+
+const arr2 = [
+    {
+        key: 'value'
+    },
+    {
+        key: 'value2'
+    },
+    {
+        key: 'value3'
+    }
+]
+// console.log(arr2[1].key);
+
+// const name = prompt("Как вас зовут?")
+// console.log("Hello " + name)
+
+// const age = alert("у вас ошибка")
+
+// const first_number = prompt ("Первое число?")
+// const last_number = prompt ("Второе число?")
+
+// if(first_number > last_number) {
+//     console.log(first_number + ">" + last_number)
+// } else if (first_number < last_number) {
+//     console.log(first_number + "<" + last_number)
+// } else {
+//     console.log(first_number + "=" + last_number)
+// }
+const age = prompt("Сколько ва лет?")
+
+if(age < 18){
+    console.log("вам нельзя входить")
+} else if (age > 18){
+    console.log("вам можно входить")
+} else {
+    console.log("ПОЖИВИ ЕЩЕ ГОДИК")
+}
+const ni = "lk"
